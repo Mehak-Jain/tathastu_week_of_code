4a0778bccf1f09e8d55b95261494390fdcf45b30
Mehak-Jain <66197804+Mehak-Jain@users.noreply.github.com>
2020-05-31 18:08:34 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+print("First number is: ")
+print(number1)
+print("Second number is: ")
+print(number2)
+number1=number1 + number2
+number2=number1 - number2
+number1=number1 - number2
+print("After swapping")
+print("First number is: ")
+print(number1)
+print("Second number is: ")
+print(number2)
